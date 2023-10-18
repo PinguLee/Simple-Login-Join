8ffe668bc689d4eac725a6f8a8722dcaff5a851d
leemingoo <dwmapi@naver.com>
2023-10-18 16:34:26 +0900

    feat: 임시 로그인 기능 구현

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 const http = require('http');
 const fs = require('fs');
+const querystring = require('querystring');
 
 const port = 8080;
 
 const ContentType = {
-  'plain' : {'Content-Type': 'text/plain'},
-  'html' : {'Content-Type': 'text/html'},
-  'css' : {'Content-Type': 'text/css'},
-  'js' : {'Content-Type': 'application/javascript'}
+  'plain': { 'Content-Type': 'text/plain' },
+  'html': { 'Content-Type': 'text/html' },
+  'css': { 'Content-Type': 'text/css' },
+  'js': { 'Content-Type': 'application/javascript' }
 };
 
 const server = http.createServer((request, response) => {
   if (request.method === 'POST' && request.url === '/') {
+    let temp = "";
+    request.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    request.on('end', () => {
+      const parseTemp = querystring.parse(temp);
+      const { id, pw } = parseTemp;
 
-
+      console.log(parseTemp);
+      console.log(id);
+      console.log(pw);
+    });
     response.writeHead(200, ContentType.plain);
     response.end('로그인 성공');
   } else if (request.method === 'GET' && request.url === '/css/style.css') {
     response.writeHead(200, ContentType.css);
-    response.end(fs.readFileSync('./css/style.css' , 'utf8'));
+    response.end(fs.readFileSync('./css/style.css', 'utf8'));
   } else if (request.method === 'GET' && request.url === '/scripts/script.js') {
     response.writeHead(200, ContentType.js);
     response.end(fs.readFileSync('./scripts/script.js', 'utf8'));
