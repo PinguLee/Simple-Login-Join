7489a4131c7ce3290f4471f26ae13c0342ed46a2
leemingoo <dwmapi@naver.com>
2023-10-18 15:37:59 +0900

    feat: pure node.js 서버 생성

---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 8080;
+
+const ContentType = {
+  'html' : {'Content-Type': 'text/html'},
+  'css' : {'Content-Type': 'text/css'},
+  'js' : {'Content-Type': 'application/javascript'}
+};
+
+const server = http.createServer((request, response) => {
+  if (request.method === 'GET' && request.url === '/') {
+    response.writeHead(200, ContentType.html);
+    response.end(fs.readFileSync('./index.html', 'utf8'));
+  } else if (request.method === 'GET' && request.url === '/static/css/style.css') {
+    response.writeHead(200, ContentType.css);
+    response.end(fs.readFileSync('./static/css/style.css' , 'utf8'));
+  } else if (request.method === 'GET' && request.url === '/static/scripts/script.js') {
+    response.writeHead(200, ContentType.js);
+    response.end(fs.readFileSync('./static/scripts/script.js', 'utf8'));
+  } else {
+    response.writeHead(404, ContentType.html);
+    response.end('404 ERROR');
+  }
+});
+
+server.listen(8080, () => {
+  console.log('서버 가동 중');
+});
