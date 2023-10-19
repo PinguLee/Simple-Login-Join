6176754eec856067c00a119c8ec792a30842b069
leemingoo <dwmapi@naver.com>
2023-10-19 17:08:39 +0900

    feat: 유효성 검사

---
+++ scripts/validation.js
@@ -0,0 +1,36 @@
+const root = document.getElementById('root');
+const btnLogin = document.getElementById('btn-login');
+const text = document.querySelectorAll('.txt');
+const txtID = document.getElementById('txt-ID');
+const txtPW1 = document.getElementById('txt-PW1');
+const txtPW2 = document.getElementById('txt-PW2');
+const txtEmail = document.getElementById('txt-Email');
+
+
+
+// 비활성화 활성화 보류
+const compare = setInterval(() => {
+  const temp = () => {
+    for (let i = 0; i< txtID.value.length; i++) {
+      txtID.value.indexOf() <= Z && txtID.value >= A
+    }
+  }
+  if (
+    (txtPW1.value === txtPW2.value)
+    &&
+    (txtID.value !== "")
+    &&
+    (txtPW1.value !== "")
+    &&
+    (txtPW2.value !== "")
+    &&
+    (txtEmail.value !== "")
+    &&
+    (txtEmail.value.indexOf("@") !== -1 || txtEmail.value.indexOf(".") !== -1)
+    
+  ) {
+    console.log("a");
+  } else {
+    console.log("a");
+  }
+}, 0);
