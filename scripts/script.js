f646be504b87205456f69f2d231fc81de950c092
leemingoo <dwmapi@naver.com>
2023-10-19 12:29:27 +0900

    feat: 값 비교 논리 연산 txt-PW 2개의 값이 같지 않으면 && txt가 비어있으면 작동 X

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,26 @@
 const root = document.getElementById('root');
 const btnLogin = document.getElementById('btn-login');
 const text = document.querySelector('.txt');
+const txtID = document.getElementById('txt-ID');
 const txtPW1 = document.getElementById('txt-PW1');
 const txtPW2 = document.getElementById('txt-PW2');
+const txtEmail = document.getElementById('txt-Email');
 
+let temp = false;
+const compare = setInterval(() => {
+  if (
+    (txtPW1.value === txtPW2.value)
+    &&
+    (txtID.value !== "")
+    &&
+    (txtPW1.value !== "")
+    &&
+    (txtPW2.value !== "")
+    &&
+    (txtEmail.value !== "")
+    &&
+    (txtEmail.value !== "")
+    ) {
+    console.log("a")
+  }
+}, 0);
